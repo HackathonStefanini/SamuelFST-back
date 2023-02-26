e60050f3fd57aea922affb1d91062222555754c6
SamuelFST <guti.samuel2002@gmail.com>
2023-02-26 16:03:53 -0300

    jogador and stefanon entities

--- src/main/java/com/stefanini/entity/Jogador.java
+++ src/main/java/com/stefanini/entity/Jogador.java
@@ -1,27 +1,41 @@
 package com.stefanini.entity;
 
-import javax.persistence.*;
 import java.math.BigDecimal;
 import java.util.ArrayList;
+import java.util.Base64;
 import java.util.List;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
+
+@Entity
 @Table(schema = "tb_jogador")
 public class Jogador {
 
     @Id
-    @Column(name = "id_jogador")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(unique = true)
+    @Column(unique = true, nullable = false)
+    @NotEmpty(message = "O nickname é obrigatório")
     private String nickname;
 
-    @Column
+    @Column(name = "password", nullable = false)
+    @NotEmpty(message = "A senha é obrigatória")
     private String password;
 
-    @Column
+    @Column(name = "balance", nullable = false)
     private BigDecimal saldo;
-
 
     @ManyToMany
     @JoinTable(name = "Jogador_Stefamon",
@@ -29,6 +43,74 @@ public class Jogador {
             inverseJoinColumns = {@JoinColumn(name = "IdStefamon")})
     private List<Stefamon> stefamons = new ArrayList<>();
 
+    @PrePersist
+	public void prePersist() {
+		password = Base64.getEncoder().encodeToString(password.getBytes());
+	}
+    
+    @PreUpdate
+	public void preUpdate() {
+		password = Base64.getEncoder().encodeToString(password.getBytes());
+	}
+     
     public Jogador() {
     }
+
+	public Jogador(Long id, @NotEmpty(message = "O nickname é obrigatório") String nickname,
+			@NotEmpty(message = "A senha é obrigatória") String password, BigDecimal saldo, List<Stefamon> stefamons) {
+		this.id = id;
+		this.nickname = nickname;
+		this.password = password;
+		this.saldo = saldo;
+		this.stefamons = stefamons;
+	}
+
+//	public static Jogador of(JogadorDTO jogadorDTO) {
+//		Jogador jogador = new Jogador();
+//		jogador.setNickname(jogadorDTO.getNickname());
+//		jogador.setPassword(jogadorDTO.getPassword());
+//		jogador.setSaldo(jogadorDTO.getSaldo());
+//		jogador.setStefamons(jogadorDTO.getStefamons());
+//		return jogador;
+//	}
+//	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNickname() {
+		return nickname;
+	}
+
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public BigDecimal getSaldo() {
+		return saldo;
+	}
+
+	public void setSaldo(BigDecimal saldo) {
+		this.saldo = saldo;
+	}
+
+	public List<Stefamon> getStefamons() {
+		return stefamons;
+	}
+
+	public void setStefamons(List<Stefamon> list) {
+		this.stefamons = list;
+	}
 }
--- src/main/java/com/stefanini/entity/Stefamon.java
+++ src/main/java/com/stefanini/entity/Stefamon.java
@@ -2,6 +2,8 @@ package com.stefanini.entity;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -10,7 +12,7 @@ import javax.persistence.Table;
 public class Stefamon {
 
     @Id
-    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable = false)
@@ -36,4 +38,92 @@ public class Stefamon {
 
     @Column(nullable = false)
     private String urlFoto;
+
+	public Stefamon() {
+	}
+
+	public Stefamon(Long id, String nome, Integer vida, Integer ataque, Integer defesa, Integer inteligencia,
+			Integer poder, Integer velocidade, String urlFoto) {
+		this.id = id;
+		this.nome = nome;
+		this.vida = vida;
+		this.ataque = ataque;
+		this.defesa = defesa;
+		this.inteligencia = inteligencia;
+		this.poder = poder;
+		this.velocidade = velocidade;
+		this.urlFoto = urlFoto;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Integer getVida() {
+		return vida;
+	}
+
+	public void setVida(Integer vida) {
+		this.vida = vida;
+	}
+
+	public Integer getAtaque() {
+		return ataque;
+	}
+
+	public void setAtaque(Integer ataque) {
+		this.ataque = ataque;
+	}
+
+	public Integer getDefesa() {
+		return defesa;
+	}
+
+	public void setDefesa(Integer defesa) {
+		this.defesa = defesa;
+	}
+
+	public Integer getInteligencia() {
+		return inteligencia;
+	}
+
+	public void setInteligencia(Integer inteligencia) {
+		this.inteligencia = inteligencia;
+	}
+
+	public Integer getPoder() {
+		return poder;
+	}
+
+	public void setPoder(Integer poder) {
+		this.poder = poder;
+	}
+
+	public Integer getVelocidade() {
+		return velocidade;
+	}
+
+	public void setVelocidade(Integer velocidade) {
+		this.velocidade = velocidade;
+	}
+
+	public String getUrlFoto() {
+		return urlFoto;
+	}
+
+	public void setUrlFoto(String urlFoto) {
+		this.urlFoto = urlFoto;
+	}
 }
